f31b4178db4f3ab402b6d3536ac81654111b2189
Олеся Сыртина <olesademcenko87091@gmail.com>
2024-12-02 23:59:40 +0300

    Добавление аскинемы в README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/olesya-syrtina/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/olesya-syrtina/frontend-project-46/actions)
+
+
+<a href="https://asciinema.org/a/s1EBxqg3IddLF8hIPvnguCecR" target="_blank"><img src="https://asciinema.org/a/s1EBxqg3IddLF8hIPvnguCecR.svg" /></a>
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,12 +1,26 @@
 import parse from './parser.js';
+import _ from 'lodash'; 
 
-const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const data1 = parse(filepath1);
-  const data2 = parse(filepath2);
-  console.log(data1);
-  console.log(data2);
-  return null;//Реализовать потом
+const genDiff = (filepath1, filepath2) => {
+  const data1 = parse(filepath1); 
+  const data2 = parse(filepath2); 
+
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+
+  const diff = keys.map((key) => {
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    }
+    return `    ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${diff.join('\n')}\n}`;
 };
 
 export default genDiff;
-
